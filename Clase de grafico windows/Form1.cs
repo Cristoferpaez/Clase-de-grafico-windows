6877bc76db505bd4d665a3bc93f1a7426f2e6238
CRISTOFER DAMIAN PAEZ PAREDES <cpaez3@indoamerica.edu.ec>
2024-06-11 15:44:42 -0500

    Agregar archivos de proyecto.

---
+++ Clase de grafico windows/Form1.cs
@@ -0,0 +1,85 @@
+namespace Clase_de_grafico_windows
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 1*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "1";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 4*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "4";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 7*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "7";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 2*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "2";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 3*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "3";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 5*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "5";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 6*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "6";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 8*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "8";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 9*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "9";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            /*corresponde al boton 0*/
+            Button button = (Button)sender;
+            textBoxRESULTADO.Text = textBoxRESULTADO.Text + "0";
+        }
+    }
+}
